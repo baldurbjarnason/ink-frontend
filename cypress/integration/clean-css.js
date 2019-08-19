11dca2f588b9dbc84eebaf33016f6cbbac9dea01
Baldur Bjarnason <bb@baldurbjarnason.com>
2019-08-19 16:37:21 -0400

    feat: serverside css sanitisation

---
+++ cypress/integration/clean-css.js
@@ -0,0 +1,21 @@
+/* eslint-env browser, mocha, chai */
+/* global cy, expect */
+
+describe("Sanitize CSS", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("basic CSS sanitisation", () => {
+    cy.request(
+      "/api/clean-css?css=%2Fdemo-epub%2Fchildrens-literature%2FEPUB%2Fcss%2Fepub.css"
+    ).then(response => {
+      const { body } = response;
+      expect(body).to.be.ok;
+      cy.fixture("epub.css").then(book1 => {
+        expect(body).to.equal(book1);
+      });
+    });
+  });
+
+});
---
+++ src/routes/api/clean-css.js
@@ -0,0 +1,26 @@
+
+import { getter } from "../../api/fetch.js";
+import {cleanCSS} from '../../api/clean-css.js'
+
+export async function get (req, res, next) {
+  const fetch = getter(req, res);
+  const file = req.query.css;
+  const url = new URL(file, "http://example.com/");
+  try {
+    const response = await fetch(file);
+    const body = await response.text();
+    const style = await cleanCSS(
+      body,
+      url.hostname === "example.com" ? url.pathname : file
+    );
+    // await fs.promises.writeFile(
+    //   "book2.json",
+    //   JSON.stringify(metadata, null, 2)
+    // );
+    console.log(style)
+    res.set('Content-Type', 'text/css')
+    return res.send(style);
+  } catch (err) {
+    next(err);
+  }
+}
