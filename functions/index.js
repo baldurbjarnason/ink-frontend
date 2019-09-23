9efe1069e6cbbdc86b558edf72fda577ef34f3b4
Baldur Bjarnason <bb@baldurbjarnason.com>
2019-09-23 17:39:04 +0000

    feat: firebase env setup.

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,16 @@
 const functions = require('firebase-functions');
+const env = functions.config()
+process.env.EPUB_PREFIX = env.epub.prefix
+process.env.API_SERVER = env.api.server
+process.env.PASSPORT_STRATEGY = env.passport.strategy
+process.env.COOKIE_KEY = env.cookie.key
+process.env.AUTH0_CLIENT_ID = env.auth0.clientid
+process.env.AUTH0_CLIENT_SECRET = env.auth0.clientsecret
+process.env.AUTH0_DOMAIN = env.auth0.domain
+process.env.CALLBACK_URL = env.callback.url
+process.env.ISSUER = env.token.issuer
+process.env.AUDIENCE = env.token.audience
+process.env.SECRETORKEY = env.token.secret
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
