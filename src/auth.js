a2f5687771a112c89f67be58068916fda686ba70
Baldur Bjarnason <bb@baldurbjarnason.com>
2019-09-25 10:59:05 +0000

    fix: turns out fixing session cookies fixed Oauth state

--- src/auth.js
+++ src/auth.js
@@ -72,8 +72,7 @@ export function setup(app) {
           domain: process.env.AUTH0_DOMAIN,
           clientID: process.env.AUTH0_CLIENT_ID,
           clientSecret: process.env.AUTH0_CLIENT_SECRET,
-          callbackURL: process.env.CALLBACK_URL,
-          state: false
+          callbackURL: process.env.CALLBACK_URL
         },
         (accessToken, refreshToken, extraParams, profile, done) => {
           return done(null, {id: profile.id});
