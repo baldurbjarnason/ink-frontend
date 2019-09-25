8f099885603092290bd3e17f935e13094b9919d6
Baldur Bjarnason <bb@baldurbjarnason.com>
2019-09-25 22:14:40 +0000

    fix: inconsistent body parsing in firebase functions

--- src/routes/api/create.js
+++ src/routes/api/create.js
@@ -1,14 +1,16 @@
 import got from "got";
 export async function post(req, res, next) {
   if (req.user && req.user.profile) {
+    if (typeof req.body !== 'string') {
+      req.body = JSON.stringify(req.body)
+    }
     try {
       const response = await got.post(req.user.profile.outbox, {
         headers: {
           "content-type": "application/ld+json",
           Authorization: `Bearer ${req.user.token}`
         },
-        body: req.body,
-        json: true
+        body: req.body
       });
       res.location(response.headers.location);
       return res.sendStatus(200);
