eb58339b037e9b2b155496a75064fb28fbbab1f6
Baldur Bjarnason <bb@baldurbjarnason.com>
2019-10-15 13:34:25 -0400

    fix: remove empty element

--- src/components/Toolbar.svelte
+++ src/components/Toolbar.svelte
@@ -1,6 +1,3 @@
-<script>
-
-</script>
 
 <style>
   .Toolbar {
--- src/doc/ChapterBody.svelte
+++ src/doc/ChapterBody.svelte
@@ -2,10 +2,6 @@
   export let html;
 </script>
 
-<style>
-
-</style>
-
 <!-- markup (zero or more items) goes here -->
 <!-- <div class="ChapterBody">{@html html }</div> -->
 <div class="ChapterBody">
